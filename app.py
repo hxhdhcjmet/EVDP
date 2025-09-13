c6aea65232d3b9317e05add3db92ec0fad3b86ea
LYJ <2871148635@qq.com>
2025-09-13 20:39:18 +0800

    调试信息

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from core.predict import DataPredict as pred
 from core import quickPlot as plt
 data=pre.load_data('d:/DeskTop/EVDP/core/data.xlsx')
 wash_data=pre.clean_data(data)
-predict=pred(wash_data,wash_data.iloc[:,0],wash_data.iloc[:,1],3)
-predict.dataInterpolate(['spline'])
-print(predict.interpolated_results)
-plot=plt.pointLineChart(predict.new_x,predict.interpolated_results['spline'],save=False)
+predict=pred(wash_data,wash_data.iloc[:,0],wash_data.iloc[:,2],3)
+predict.dataInterpolate(['linear'])
+
+
+
+
+
 
 
 
