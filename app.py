24d91774b79eb5d1bb1996c6189a5bff579c8a48
LYJ <2871148635@qq.com>
2025-09-27 09:43:46 +0800

    app页面调式代码

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from core import visualize as vs
 from core import PreProcessing as pre
 from core.predict import DataPredict as pred 
 from core import quickPlot as plt
-data=pre.load_data('d:/DeskTop/EVDP/core/data.xlsx')
-wash_data=pre.clean_data(data)
-predict=pred(wash_data,wash_data.iloc[:,0],wash_data.iloc[:,2],3)
-predict.dataInterpolate(['linear'])
+data=pre.load_data('d:/DeskTop/test.xlsx')
+wahsed_data=pre.clean_data(data)
+prediction=pred(wahsed_data,wahsed_data.iloc[:,1],wahsed_data.iloc[:,2])
+prediction.train_model('random_forest')
+prediction.visualize_predict_result(wahsed_data.iloc[:,1],wahsed_data.iloc[:,2],prediction.prediction_results['random_forest'],'random_forest')
+
 
 
 
