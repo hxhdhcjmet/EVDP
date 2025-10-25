e54048b7b1c2940ed1e5bb33d7fea5f5a6e2617f
lyj <2871148635@qq.com>
2025-10-25 23:23:32 +0800

    添加playwright获取多日天气数据功能

--- core/spider.py
+++ core/spider.py
@@ -6,6 +6,7 @@ import requests
 import time 
 import re
 import json
+from playwright.sync_api import sync_playwright
 
 url_weather = r"https://d1.weather.com.cn/sk_2d/101010100.html?_=1761118015460"
 
@@ -24,7 +25,7 @@ class Weather:
         wet:湿度
         """
         self.city = city
-        self.upgrade_Time = upgradeTime
+        self.upgrade_Time = upgrade_Time
         self.temp = temp
         self.wind_position = wind_position
         self.wind = wind
@@ -62,9 +63,9 @@ def get_1dweather_data(city_code):
     if response.status_code == 200:
         # 200-OK
         json_data = response.text.replace("var dataSK=","").strip()
-        print(type(json_data))
+        
         data = json.loads(json_data)
-        print(type(data))
+
         city_weather = Weather(data.get("cityname"),data.get("time"),data.get("temp"),data.get("WD"),data.get("WS"),data.get("SD"))
         return city_weather
        
@@ -78,36 +79,35 @@ def get_7dweather_data(city_code):
     获取7日天气
     city_code:城市代码
     """
+    # 获取7日找api接口失败，跳转商业合作界面，遂用playwright
+    with sync_playwright() as p:
+        # 使用playwright框架
+        # 配置浏览器
+        browser = p.chromium.launch(headless = True)
+        page = browser.new_page()
 
-    url = f"https://d1.weather.com.cn/weather/7d/{city_code}.html?_={int(time.time()*1000)}"
-    headers = {
-    "Referer": f"https://www.weather.com.cn/weather/{city_code}.shtml",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/129.0.0.0 Safari/537.36",
-    "Accept": "*/*",
-    "Accept-Language": "zh-CN,zh;q=0.9",
-    "Connection": "keep-alive",
-    "Host": "d1.weather.com.cn",
-    "X-Requested-With": "XMLHttpRequest",
-    "Accept-Encoding": "gzip, deflate, br"
-}
-    print("开始获取数据...")
-    time.sleep(1)
-    response = requests.get(url,headers=headers,allow_redirects = False)
-    response.encoding = "utf-8"
-    
-    
-    if True:
-        #200-OK
-        print(response.status_code)
-        print(response.url)
-        print("Location:",response.headers.get("Location"))
-        print(response.text[:300])
-    else:
-        print(f"请求失败,状态码:{response.status_code}")
+        target_url = f"https://www.weather.com.cn/weather/{city_code}.shtml"
+        page.goto(target_url)
+
+        page.wait_for_timeout(1000) # 等待1s确保所有元素加载完成
+
+        page_html = page.content() # 获取渲染后的完整html
+
+        soup = BeautifulSoup(page_html,"html.parser")
+
+        tomorrow_t = soup.select(r"#\37 d > ul > li:nth-child(2) > p.tem > span")
+        if tomorrow_t is not None:
+            print(tomorrow_t[0].text)
+        else:
+            print("空")
+        
+        browser.close()
+
+
+        
 
 
 
 beijing = 101010100
-get_7dweather_data(beijing)
+qingdao = 101120201
+get_7dweather_data(qingdao)
